69a7e3107289b9a31431a094a4b0621aca286f9a
Anastasia <adudarenko@google.com>
2026-04-08 11:01:01 +0200

    feat: setup subrouter for pub/sub trigger (#704)
    Create a new subrouter that will handle the pubsub trigger endpoint and add the implementation for this trigger.

--- cmd/launcher/full/full.go
+++ cmd/launcher/full/full.go
@@ -23,10 +23,11 @@ import (
 	"google.golang.org/adk/cmd/launcher/web/a2a"
 	"google.golang.org/adk/cmd/launcher/web/agentengine"
 	"google.golang.org/adk/cmd/launcher/web/api"
+	"google.golang.org/adk/cmd/launcher/web/triggers/pubsub"
 	"google.golang.org/adk/cmd/launcher/web/webui"
 )
 
 // NewLauncher returnes the most versatile universal launcher with all options built-in.
 func NewLauncher() launcher.Launcher {
-	return universal.NewLauncher(console.NewLauncher(), web.NewLauncher(webui.NewLauncher(), a2a.NewLauncher(), api.NewLauncher(), agentengine.NewLauncher()))
+	return universal.NewLauncher(console.NewLauncher(), web.NewLauncher(webui.NewLauncher(), a2a.NewLauncher(), pubsub.NewLauncher(), api.NewLauncher(), agentengine.NewLauncher()))
 }
